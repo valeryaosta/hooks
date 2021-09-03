65913883bdea894b543dca032a4d0f2a9d64daae
valeryaosta <gerasimenko.valery@gmail.com>
2021-09-03 14:17:07 +0300

    useEffect + Class life-cycle methods

--- src/List.jsx
+++ src/List.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-class List extends React.Component {
+/*class List extends React.Component {
 
     state = {
         numbers: [1, 2, 3],
@@ -44,28 +44,59 @@ class List extends React.Component {
             </div>
         );
     }
-}
+}*/
 
-// const List = () => {
-//
-//     const [numbers, setNumbers] = useState([1, 2, 3]);
-//
-//     const addNumber = () => {
-//         const newNumber = Math.floor(Math.random() * 10);
-//         const newArr = [...numbers, newNumber];
-//         setNumbers(newArr);
-//     };
-//
-//     return (
-//         <div>
-//             <ul>
-//                 {numbers.map((num, index) => (
-//                     <li key={index}>{num}</li>
-//                 ))}
-//             </ul>
-//             <button onClick={addNumber}>Новое число</button>
-//         </div>
-//     );
-// }
+const List = () => {
+
+    const [numbers, setNumbers] = useState([1, 2, 3]);
+    const [count, setCount] = useState(0);
+
+    const addNumber = () => {
+        const newNumber = Math.floor(Math.random() * 10);
+        const newArr = [...numbers, newNumber];
+        setNumbers(newArr);
+    };
+    //когда произойдет какое-то действие(К была первый раз отображена или обновится,удалится)
+    // я могу сделать что-то!! и вот это что-то будех раниться в коллбэке юзЭффекта
+    // deps [] - 1 time --> ComponentDidMount + ComponentWillUnmount
+    // deps  - each time any case (1 render, updates) --> ComponentDidUpdate
+    // deps [numbers] - if numbers changed  --> ComponentDidUpdate(для numbers)
+
+    //ComponentDidUpdate только для numbers
+    useEffect(() => {
+        console.log("NUMBERS LIST UPDATED");
+    }, [numbers]);
+    //ComponentDidUpdate только count
+    useEffect(() => {
+        console.log("COUNT LIST UPDATED");
+    }, [count]);
+    //ComponentDidUpdate
+    useEffect(() => {
+        console.log("COMPONENT UPDATED");
+    },);
+    //ComponentDidMount + ComponentWillUnmount
+    useEffect(() => {
+        console.log("DID MOUNT");
+        return () => {
+            console.log("WILL UNMOUNT");
+        }
+    }, []);
+
+
+    return (
+        <div>
+            <div>
+                <h1>{count}</h1>
+                <button onClick={() => setCount(count + 1)}>+</button>
+            </div>
+            <ul>
+                {numbers.map((num, index) => (
+                    <li key={index}>{num}</li>
+                ))}
+            </ul>
+            <button onClick={addNumber}>NEW NUMBER</button>
+        </div>
+    );
+}
 
 export default List;
