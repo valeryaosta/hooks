88ab8a8c3e8bcadce66350525899d261091a05bc
valeryaosta <gerasimenko.valery@gmail.com>
2021-09-03 12:51:35 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [numbers, setNumbers] = useState([1, 2, 3]);
+
+    const addNumber = () => {
+        const newNumber = Math.floor(Math.random() * 10);
+        const newArr = [...numbers, newNumber];
+        setNumbers(newArr);
+    };
+
+    return (
+        <div className="App">
+            <ul>
+                {numbers.map((num, index) => (
+                    <li key={index}>{num}</li>
+                ))}
+            </ul>
+            <button onClick={addNumber}>Новое число</button>
+        </div>
+    );
 }
 
 export default App;
